04a6756fac4612d2cf8decaf213041ab2f9a7f9c
delmas007 <azertyazerty1ze@gmail.com>
2025-06-27 07:50:10 +0000

    init

---
+++ src/main/java/com/example/avancedrag/AvancedRagApplication.java
@@ -0,0 +1,13 @@
+package com.example.avancedrag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvancedRagApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AvancedRagApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/avancedrag/AvancedRagApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.avancedrag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvancedRagApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
